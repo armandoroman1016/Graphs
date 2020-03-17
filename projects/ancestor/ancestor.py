470d2b02bda775552081ea535239fbb75bb3ac18
Armando Roman <armandoroman1016@gmail.com>
2020-03-17 14:51:01 -0700

    all tests pass

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -5,7 +5,7 @@ def earliest_ancestor(ancestors, starting_node):
     visited = set()
 
     # create a dictionary with earliest ancestor/ set key to depth and value to idx
-    earliest_ancestor = {}
+    ancestor_history = {}
 
     ancestor_adjacency = {}
     # loop through ancestors and store in dictionary with key being ancestor[1] and val being ancestor[0] 
@@ -23,8 +23,25 @@ def earliest_ancestor(ancestors, starting_node):
 
     # create helper function
     def earliest_ancestor_helper(current_vertex, count = 0):
-        pass
-    
-    return max(earliest_ancestor)
 
+        # base case - starting node is not in ancestor_adjacency list 
+        if current_vertex not in ancestor_adjacency: 
+                #if count is already in dictionary compare value and use lower value
+                if count in ancestor_history:
+                    ancestor_history[count] = ancestor_history[count] if ancestor_history[count] > current_vertex else current_vertex
+
+                # else add to dictionary with count set as key and current vertex as value
+                else:
+                    ancestor_history[count] = current_vertex
+                
+                return
+
+        # iterate through and recursively call on the current vertex ancestors
+        for ancestor in ancestor_adjacency[current_vertex]:
+            count += 1
+            earliest_ancestor_helper(ancestor, count)
+    
+    earliest_ancestor_helper(starting_node)
+
+    return ancestor_history[max(ancestor_history)]
 
